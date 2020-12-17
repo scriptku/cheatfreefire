2494bd1c984dedd2f14d04f293dcf6dda58cfafe
Script-Ku <75932145+scriptku@users.noreply.github.com>
2020-12-17 10:47:11 +0700

    Create cheat.sh

---
+++ cheat.sh
@@ -0,0 +1,71 @@
+#!/system/xbin/bash
+#mau Recode izin dulu di Instagram Ridwan58aja
+#Github:https://github.com/RidwanKechil
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo
+echo $ku "Selamat Datang Di Github Gw"
+echo "Masukkan Kode Script Nya"
+read -p "root@Kode Script > " bro
+if [ $bro = 13325 ] || [ $bro = 13325 ]
+then
+
+sleep 2
+echo $me "Nih Broo Dukung Scriptku dan Channel yt Gw Ya Broo"
+sleep 2
+echo $cy "########################"
+echo "##" $pu "subscribe channel gw broo"
+sleep 2
+termux-open https://youtube.com/channel/UCAdKWwRiRs35dLFkt1s3LHg
+sleep 6
+echo $cy "##" $pu "Follow Instagram Gw"
+sleep 2
+termux-open https://instagram.com/scriptku
+sleep 8
+echo $cy "#########################"
+echo $ku "Terimakasih " $nick
+echo
+echo
+clear
+
+fi
+
+sleep 3
+echo "Pilih Salah Satu Broo"
+echo $ku "1. Cheat afree fire Mod Menu"
+echo "2. Tambah  Damage "
+echo "Work Dua duA Nya"
+echo "pilih broo"
+
+read -p "[pilih 1/2]> " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+echo
+echo
+clear
+
+echo "Sabar Broo"
+echo $ku "Langsung Download aja broo "
+sleep 2
+echo $me "No Password"
+termux-open https://www.mediafire.com/download/vbtv0ble3susrlx
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+
+echo $ku "script Tambah Damage Free Fire"
+echo $bi "Password :SayankKamu"
+sleep 2
+echo $ku "estrak pakek z Archirver"
+sleep 4
+termux-open https://www.mediafire.com/download/ihfaewe1rkknpvu
+fi
+
+echo " makasih" $pu
